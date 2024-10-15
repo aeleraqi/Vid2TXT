2cc60f845fce8e991c59c56f313d13b38c6eb9e6
Bashamega <adambashaahmednaji@gmail.com>
2024-10-15 19:20:28 +0300

    feat: lib integration

--- gui/main.py
+++ gui/main.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from lib.selectFile import select_file
+from lib.audio_processing import process_audio_file
 import os
 
 data = {
@@ -7,19 +8,18 @@ data = {
     "language": ""
 }
 
-def open_file():
-    try:
-        # Attempt to select a file
-        file_path = select_file()
-        # Update the data dictionary with the selected file path
-        data.update({'file_url': file_path})
-        # Update the button text to show the selected file path
-        file_name = os.path.basename(file_path)
-        open_button.configure(text=file_name)
-    except FileNotFoundError:
-        # Display the error message if no file was selected
-        print("No file")
+def proces():
+    output = process_audio_file(data["file_url"], data["language"])
+    print("output: "+ output)
 
+def open_file():
+    # Attempt to select a file
+    file_path = select_file()
+    # Update the data dictionary with the selected file path
+    data.update({'file_url': file_path})
+    # Update the button text to show the selected file path
+    file_name = os.path.basename(file_path)
+    open_button.configure(text=file_name)
 # Set the appearance and theme
 ctk.set_appearance_mode("dark")  # Modes: system (default), light, dark
 
